0da4cfb048248c29edc3d4037c021f8bee5fff64
Vishnu Vardhan <vishuchappidi12@gmail.com>
2025-01-03 22:18:36 +0530

    My First day SQL commands

---
+++ SQL_BASICS/SQL_Basic_Commands.sql
@@ -0,0 +1,9 @@
+-- SQL Command to create a DATABASE --
+create database Training;
+
+-- SQL Command to use the created database --
+use training;
+
+-- sql coomand to create table --
+create table student(name varchar(20),age int,place varchar(20));
+
