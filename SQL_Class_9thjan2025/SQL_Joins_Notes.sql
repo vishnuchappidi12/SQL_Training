876e8e814443e625e486ac9a79bf9bfd84e592d6
Vishnu Vardhan <vishuchappidi12@gmail.com>
2025-01-09 18:03:59 +0530

    SQL_joins

---
+++ SQL_Class_9thjan2025/SQL_Joins_Notes.sql
@@ -0,0 +1,12 @@
+-------------------------------------------------------------------
+------------------------JOINS IN SQL----------------------
+1.Cartesian Product : Every record in a tbale will join with every record in another table.
+2.Inner join: Every record in a table join with matching records in other table, Based on common Feild.  
+3.Left join: common records from the both the tables, additional records from left side table.
+4.Right Join: Common records from both the tables,remain records from right side table.
+5.Full Join: Common records from both the tables, Left out records from the both the tables.
+6.Conditional join: Using Some particular condidtion. 
+7.Self join: If table join itself is called self join.
+8.Date operations: performing a dates.
+
+---------------------------------------------------------------------
